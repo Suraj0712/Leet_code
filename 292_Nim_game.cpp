dbd4a0f1e269a6fe76a3ecf5679335d25254a5c8
Suraj0712 <sssapkal@wpi.edu>
2020-05-28 01:36:32 -0400

    Solution for prob no 292

---
+++ 292_Nim_game.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class Solution {
+public:
+    bool canWinNim(int n) 
+    {
+        if(n%4==0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+        
+    }
+};
