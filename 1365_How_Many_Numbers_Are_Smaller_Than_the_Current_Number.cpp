80097d9b2f99c7ac01bf03ef35681e4df2a4a55c
Suraj0712 <sssapkal@wpi.edu>
2020-06-01 23:56:06 -0400

    solution to 1365

---
+++ 1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) 
+    {        
+        vector<int> copy = nums;
+        sort(copy.begin(), copy.end());
+        vector<int> v;
+        auto it = copy.begin();
+        
+        for(auto i : nums)
+        {
+            it = lower_bound(copy.begin(), copy.end(), i);
+            v.push_back(it-copy.begin());
+        }
+
+        return v;
+    }
+};
