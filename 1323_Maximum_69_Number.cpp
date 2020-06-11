324ee67e15cc63059131cab607502f6b5c3a9d22
Suraj0712 <sssapkal@wpi.edu>
2020-06-11 00:10:27 -0400

    solution for 1323 efficient one

--- 1323_Maximum_69_Number.cpp
+++ 1323_Maximum_69_Number.cpp
@@ -1,27 +1,40 @@
-class Solution {
-public:
-    int maximum69Number (int num) 
-    {
-        int order=0;
-        int temp =num;
-        int val=0;
+// class Solution {
+// public:
+//     int maximum69Number (int num) 
+//     {
+//         vector<int> digits;
+//         int ret_var=0;
+//         bool flag =true;
         
-        while(num/10>10)
-        {
-            num=num/10;
-            order++;
-            cout<<num<<"\n";
-        }
-        order++;
-        cout<<order;
+//         while(num>0)
+//         {
+//             digits.push_back(num%10);
+//             num = num/10;
+            
+//         }
         
-        val = temp/pow(10,order);
-        
-             
-        while(val!=6 or )
-        
-        return num;
-        
-    }
-};
+//         for(auto i= digits.rbegin() ; i!=digits.rend(); ++i)
+//         {
+//             if(*i ==6 and flag)
+//             {
+//                 *i=9;
+//                 flag=false;
+//             }
+//             ret_var = ret_var*10+*i;            
+//         }         
+//         return ret_var;      
+//     }
+// };
 
+    string s = to_string(num) ;
+    
+    for (int i = 0 ; i < s.size() ; i++)
+    {
+        if (s[i] == '6')
+        {
+            s[i] = '9' ;
+            break ;
+        }
+    }
+    return stoi(s) ;
+}
